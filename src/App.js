1d09c6eef58dfdebd30b00149ec7ac90c5be1681
Richard Morgan <>
2023-01-06 15:30:07 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Rich's static React.js app hosted in AWS.
+          Ela's static React.js app hosted in AWS.
         </p>
         <a
           className="App-link"
